955131035d6b07c3b111a61586eddc5dcdbf91bf
Sosa Edison <me@sosaedison.io>
2022-09-25 14:27:00 -0500

    api and web are in docker and connected

--- web/chatappweb/src/App.js
+++ web/chatappweb/src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [helloText, setHelloText] = useState("None");
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:8000/", {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setHelloText(data);
+      })
+      .catch((err) => {
+        setHelloText("Nothing came back");
+      });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{helloText}</h1>
     </div>
   );
 }
