afa2e2e692e8440d94ce65a6b73eb3b5f744c51f
Sosa Edison <me@sosaedison.io>
2022-10-01 10:53:39 -0500

    messages are sent to a new user when they connect

--- api/chatappapi/main.py
+++ api/chatappapi/main.py
@@ -1,12 +1,16 @@
-from datetime import datetime
 from fastapi import FastAPI, WebSocket
 from fastapi.middleware.cors import CORSMiddleware
+from websockets.exceptions import ConnectionClosedError
+from starlette.websockets import WebSocketDisconnect
+
 
 from websocket_manager import ConnectionManager
 
 from utils import now_as_str
 
 manager = ConnectionManager()
+
+chat_db = []
 
 app = FastAPI()
 
@@ -22,6 +26,11 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+"""
+- when a new user connects, they should be sent all the messages that they don't already have 
+- the messages only live while the app is live
+"""
+
 
 @app.get("/")
 def home():
@@ -30,7 +39,13 @@ def home():
 
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
-    await manager.connect(websocket=websocket)
-    while True:
-        data = await websocket.receive_text()
-        await manager.broadcast_json({"msg": data, "created": now_as_str()})
+    await manager.connect(websocket=websocket, messages=chat_db)
+    try:
+        while True:
+            print("RECEIVING AND SENDING MESSAGES")
+            data = await websocket.receive_text()
+            msg = {"msg": data, "created": now_as_str()}
+            chat_db.append(msg)
+            await manager.broadcast_json(msg)
+    except (ConnectionClosedError, WebSocketDisconnect) as ex:
+        manager.disconnect(websocket=websocket)
--- api/chatappapi/websocket_manager.py
+++ api/chatappapi/websocket_manager.py
@@ -6,11 +6,18 @@ class ConnectionManager:
     def __init__(self):
         self.active_connections: List[WebSocket] = []
 
-    async def connect(self, websocket: WebSocket):
+    async def connect(self, websocket: WebSocket, messages: List):
         await websocket.accept()
         self.active_connections.append(websocket)
+        print("NUM CONNECTIONS:", len(self.active_connections))
+
+        print("AVAILABLE MESSAGES", len(messages))
+        for m in messages:
+            print("Message", m)
+            await self.broadcast_json(m)
 
     def disconnect(self, websocket: WebSocket):
+        print("DISCONNECT CALLED")
         self.active_connections.remove(websocket)
 
     async def send_personal_message(self, message: str, websocket: WebSocket):
@@ -22,4 +29,5 @@ class ConnectionManager:
 
     async def broadcast_json(self, data: object):
         for connection in self.active_connections:
+            print("MESSAGE BROADCASTED", data)
             await connection.send_json(data=data)
